05677049ea29bb2a6923d38ffb7a2be969e26acc
Mhmd0Mhmod <mohamedmahmoudf.2002@gmail.com>
2025-12-03 12:04:58 +0200

    feat: remove CI and deploy workflows, add category type definition

---
+++ types/category.ts
@@ -0,0 +1,10 @@
+interface Category {
+  id: number;
+  name: string;
+  description: string;
+  countryId: number;
+  countryName: string;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
